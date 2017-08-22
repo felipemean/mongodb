98a744ae8d66a2463a988346b7ac0c134572c727
Hutz3\Felipe Gil <ab4795@gmail.com>
2017-08-22 10:07:08 +0200

    Fichero de WAYNE Dyer Reflexiones

--- C/Resueltos/Complementos del Curso, Ejercicios Resueltos y más/11 Cadenas/3 Utilización de String.h/Problema 14.c
+++ C/Resueltos/Complementos del Curso, Ejercicios Resueltos y más/11 Cadenas/3 Utilización de String.h/Problema 14.c
@@ -3,19 +3,32 @@ caracteres tiene.*/
 
 #include<stdio.h>
 #include<string.h>
+#include<conio.h>
 
 int main(){
 	char palabra[30];
-	
+	char palabra1[20], palabra2[20];
+
 	printf("Digite una palabra: "); //Pedimos la cadena al usuario
 	gets(palabra);
 	
 	strrev(palabra);  //invertimos la palabra con la funcion strrev
-	
+	 
 	printf("Cadena Invertida: %s",palabra); //Imprimimos la cadena invertida
-	printf("\nCaracteres totales: %i",strlen(palabra)); //Mostramos la cantidad de 
+	printf("\nCaracteres totales: %i\n",strlen(palabra)); //Mostramos la cantidad de 
 														//caracteres que tiene.
+	getch();
+		printf("Digite una palabra: "); //Pedimos la cadena al usuario
+	gets(palabra1);
+		printf("Digite una palabra: "); //Pedimos la cadena al usuario
+	gets(palabra2);
 	
+	strupr(palabra1);
+	strupr(palabra2);
+	printf("\n\t Las palabras invertidas son:\n\t Palabra1:  %s\t y   Palabra2:	%s", palabra1,palabra2);
 	
+	if(strcmp(palabra1,palabra2) ==0 ){
+		printf("\n\t Las dos palabras son IDENTICAS");
+	}
 	return 0;
 }
